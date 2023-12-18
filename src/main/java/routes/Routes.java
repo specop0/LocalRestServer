25e71529ccb4874e0c94b17da45316b3a3a22086
SpecOp0 <SpecOp0@yahoo.de>
2023-12-18 12:10:24 +0100

    feat: default endpoint message

--- src/main/java/routes/Routes.java
+++ src/main/java/routes/Routes.java
@@ -7,6 +7,7 @@ public class Routes {
     public static void EstablishRoutes(IDatabase database, String ipAddress, int port) {
         spark.Spark.ipAddress(ipAddress);
         spark.Spark.port(port);
+        spark.Spark.get("/", (req, res) -> "LocalRestServer is running");
         spark.Spark.get("/data/*/*", "application/json", new GetDataRoute(database));
         spark.Spark.put("/data/*/*", "application/json", new SetDataRoute(database));
         spark.Spark.delete("/data/*/*", "application/json", new DeleteDataRoute(database));
